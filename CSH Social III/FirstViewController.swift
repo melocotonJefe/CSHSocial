4a886b41891d71c43494b3141bc24e8fe2cfe912
Jeffrey Phillips <jrphillips718@gmail.com>
2018-02-24 12:20:26 -0800

    Initial Commit

---
+++ CSH Social III/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  CSH Social III
+//
+//  Created by Tino Martino on 2/18/18.
+//  Copyright © 2018 Computer Science House. All rights reserved.
+//
+
+import UIKit
+
+
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CSH Social III/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CSH Social III
+//
+//  Created by Tino Martino on 2/18/18.
+//  Copyright © 2018 Computer Science House. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
