dcb63f1426247d9083473f1128c5309642943ed6
Jeffrey Phillips <jrphillips718@gmail.com>
2018-02-25 13:29:23 -0500

    Transfer Event and Event Data Source

---
+++ CSH Social V/CSH Social V/Event.swift
@@ -0,0 +1,18 @@
+//
+//  Event.swift
+//  CSH Social III
+//
+//  Created by Jeff Phillips on 2/24/18.
+//  Copyright © 2018 Computer Science House. All rights reserved.
+//
+
+import Foundation
+
+struct Event {
+    var title: String!
+    
+    init(title: String) {
+        self.title = title
+    }
+    
+}
