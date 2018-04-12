d01efb7eb3a6c0dda2f5f8aa8c4fc3b5d5aeb33e
Cori Mori <cori1753@gmail.com>
2018-04-12 17:03:01 -0400

    Started work on local data storage

--- CSH Social V/CSH Social V/SecondViewController.swift
+++ CSH Social V/CSH Social V/SecondViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import SQLite
 
 class SecondViewController: UIViewController, UITextFieldDelegate{
 
+    var database: Connection!
+    let eventTable = Table("events")
+    let eventName = Expression<String>("name")
+    let eventTime = Expression<String>("time")
+    let eventLocation = Expression<String>("location")
+    let eventAbout = Expression<String>("about")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do{
+            //Sets up file to store the database in
+            let documentDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            
+            //Creates path to the file
+            let fileURL = documentDirectory.appendingPathComponent("events").appendingPathExtension("sqlite3")
+            
+            //Trys to connect the database to the file it will be stored in
+            let database = try Connection(fileURL.path)
+            self.database = database
+        }catch{
+            print(error)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +51,21 @@ class SecondViewController: UIViewController, UITextFieldDelegate{
     @IBAction func Submit(_ sender: UIButton) {
         let event = Event(title:eventNameField.text!,date:timeField.text!,location:locationField.text!,about:aboutField.text!)
         EventDataSource.createEvent(event: event)
+        
+        let createTable = self.eventTable.create { (table) in
+            table.column(self.eventName, primaryKey: true)
+            table.column(self.eventTime)
+            table.column(self.eventLocation)
+            table.column(self.eventAbout)
+        }
+        
+        do{
+            try self.database.run(createTable)
+            print("Created Table")
+        }catch{
+            print(error)
+        }
+        
         
         //clear the text fields on submit
         eventNameField.text!=""
